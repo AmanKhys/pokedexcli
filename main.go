c23292bb2d43333e28556b2b649488c541a6f310
amankhys <amankhys2112@gmail.com>
2023-12-07 23:34:35 +0530

    create a func which return map of commands and a repl to execute commands.

---
+++ main.go
@@ -0,0 +1,67 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+)
+
+type cliCommand struct {
+	name        string
+	description string
+	callback    func() error
+}
+
+func main() {
+
+	fmt.Println("Hello World!")
+	scanner := bufio.NewScanner(os.Stdin)
+	for {
+		fmt.Print("pokedex >>")
+		scanner.Scan()
+		commandString := scanner.Text()
+		fmt.Printf("the command %v is used \n", commandString)
+		fmt.Println()
+		command := commandProvider()[commandString]
+		err := command.callback()
+
+		fmt.Println()
+		fmt.Println()
+		if err != nil {
+			fmt.Println(err)
+		}
+	}
+
+}
+
+func commandProvider() map[string]cliCommand {
+
+	commandHelp := func() error {
+		fmt.Println("Welcome to Pokedex!")
+		fmt.Println("Usage:")
+		commands := commandProvider()
+		for _, value := range commands {
+			fmt.Printf("%v: %v \n", value.name, value.description)
+		}
+		return nil
+
+	}
+
+	commandExit := func() error {
+		os.Exit(0)
+		return nil
+	}
+
+	return map[string]cliCommand{
+		"help": {
+			name:        "help",
+			description: "Displays a help message",
+			callback:    commandHelp,
+		},
+		"exit": {
+			name:        "exit",
+			description: "Exit the Pokedex",
+			callback:    commandExit,
+		},
+	}
+}
